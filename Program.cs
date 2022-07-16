8a8288faa252d9a6ea361ab4f91086208bd25cdf
danang507 <danangwijayanto507@gmail.com>
2022-07-16 10:08:55 +0700

    Responsi

---
+++ Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProjectMahasiswa
+{
+    class Program
+    {
+        // deklarasi objek collection untuk menampung objek mahasiswa
+        static List<Mahasiswa> daftarMahasiswa = new List<Mahasiswa>();
+
+        static void Main(string[] args)
+        {
+            Console.Title = "Responsi UAS Matakuliah Pemrograman";
+
+            while (true)
+            {
+                TampilMenu();
+
+                Console.Write("\nNomor Menu [1..3]: ");
+                int nomorMenu = Convert.ToInt32(Console.ReadLine());
+
+                switch (nomorMenu)
+                {
+                    case 1:
+                        TambahMahasiswa();
+                        break;
+
+                    case 2:
+                        TampilMahasiswa();
+                        break;
+
+                    case 3: // keluar dari program
+                        return;
+
+                    default:
+                        break;
+                }
+            }
+        }
+
+        static void TampilMenu()
+        {
+            Console.Clear();
+            Console.WriteLine("Pilih Menu Aplikasi\n");
+            Console.WriteLine("1. Tambah Mahasiswa");
+            Console.WriteLine("2. Tampilkan Mahasiswa");
+            Console.WriteLine("3. Keluar");
+
+            // PERINTAH: lengkapi kode untuk menampilkan menu
+        }
+
+        static void TambahMahasiswa()
+        {
+            Console.Clear();
+            Mahasiswa Mahasiswa = new Mahasiswa();
+
+            // PERINTAH: lengkapi kode untuk menambahkan objek mahasiswa ke dalam collection
+
+            Console.Write("NIM: ");
+            Mahasiswa.Nim = Console.ReadLine();
+            Console.Write("Nama: ");
+            Mahasiswa.Nama = Console.ReadLine();
+            Console.Write("Jenis Kelamin [L/P] : ");
+            Mahasiswa.Gender = Console.ReadLine();
+            if (Mahasiswa.Gender == "L")
+            {
+                Mahasiswa.Gender = "Laki-Laki";
+            }
+            else if (Mahasiswa.Gender == "P")
+            {
+                Mahasiswa.Gender = "Perempuan";
+            }
+            Console.Write("IPK : ");
+            Mahasiswa.Ipk = Console.ReadLine();
+
+            daftarMahasiswa.Add(Mahasiswa);
+
+            Console.WriteLine("\nTekan ENTER untuk kembali ke menu");
+            Console.ReadKey();
+        }
+
+        static void TampilMahasiswa()
+        {
+            Console.Clear();
+
+            // PERINTAH: lengkapi kode untuk menampilkan daftar mahasiswa yang ada di dalam collection
+
+            Console.WriteLine("Data Mahasiswa\n");
+            int no = 1;
+
+            
+            foreach (Mahasiswa mahasiswa in daftarMahasiswa)
+            {
+
+                Console.WriteLine("{1}, {2} , {3} ,{4} ", no, mahasiswa.Nim, mahasiswa.Nama, mahasiswa.Gender, mahasiswa.Ipk);
+                no++;
+            }
+
+            Console.WriteLine("\nTekan enter untuk kembali ke menu");
+            Console.ReadKey();
+        }
+    }
+}
+
